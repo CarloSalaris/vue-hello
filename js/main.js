7ab00336682e79f6bcc36f7ca0a1b91e969df1cf
CarloSalaris <carloa.salaris@gmail.com>
2023-05-18 17:33:29 +0200

    Bonus+ all starts hidden and you toggle it with the button and through the img

--- js/main.js
+++ js/main.js
@@ -10,7 +10,19 @@ createApp({
         return {
             message:'Hello World!',
             imageSrc:'img/clay-banks-8q6e5hu3Ilc-unsplash.jpg',
-            imageDescription:'image from Clay Banks (unsplash)'
+            imageDescription:'image from Clay Banks (unsplash)',
+            isHidden: true,
+            HideShowDecl:'SHOW'
+        }
+    },
+    methods: {
+        toggleHello() {
+            if (this.isHidden === false) {
+                this.HideShowDecl = 'SHOW';
+        } else {
+            this.HideShowDecl = 'HIDE';
+        }
+            this.isHidden = !this.isHidden;
         }
     }
 }).mount("#app")
